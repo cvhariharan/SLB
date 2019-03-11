37df110435412b96947db000be284496ffa62722
Hariharan <cvhariharan@protonmail.com>
2019-03-11 14:45:32 +0530

    Primary logging

--- main.go
+++ main.go
@@ -37,12 +37,18 @@ func handle(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	logFile, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	logger := log.New(logFile, "", log.LstdFlags)
+
 	var configFile = "./config.json"
 	if len(os.Args) > 1 {
 		configFile = os.Args[1]
 	}
 	fmt.Println(configFile)
-
 	data, err := ioutil.ReadFile(configFile)
 	err = json.Unmarshal(data, &config)
 	if err != nil {
@@ -58,6 +64,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", handle)
-	fmt.Println("Port" + port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	logger.Println("Port: " + port)
+	logger.Println("Starting server...")
+	logger.Fatal(http.ListenAndServe(port, nil))
 }
